daccd026895e833f028dc4998e81526dc555f8d6
Anna Halii <annahalii@Annas-MacBook-Pro.local>
2022-01-09 22:31:09 +0200

    fix: fixed imports

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import AdditionalData
 
 admin.site.register(AdditionalData)
 
--- user_app/urls.py
+++ user_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import IndexView, ListOfVisitors
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
